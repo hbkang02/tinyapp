56e1eeb0c3fc5e6589b4d6be74e40a4433531f9d
John Kang <hbkang02@gmail.com>
2022-11-29 00:44:50 -0800

    URL shortening part 1

--- express_server.js
+++ express_server.js
@@ -3,15 +3,25 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: 'http://www.lighthouselabs.ca' };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.get("/urls", (req, res) => {
@@ -35,3 +45,6 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n')
 });
 
+function generateRandomString() {
+  
+}
