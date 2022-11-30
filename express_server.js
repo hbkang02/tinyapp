cd799cc8e84699a8e52e53dfc437ea9d195f8f7c
John Kang <hbkang02@gmail.com>
2022-11-30 13:21:58 -0800

    update to the server: delete

--- express_server.js
+++ express_server.js
@@ -5,33 +5,67 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+
+//data
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
+
+function generateRandomString() {
+  let r = (Math.random() + 1).toString(36).substring(7);
+  return r;
+};
+
+
+//////////////////////////////////////////////////////////////////////////
+
+app.get('/', (req, res) => {
+  res.send("Hello!");
+});
+
+
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new")
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: 'http://www.lighthouselabs.ca' };
+  const shortURL = req.params.id;
+  const templateVars = { id: shortURL, longURL: urlDatabase[shortURL] };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
-});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get('/', (req, res) => {
-  res.send("Hello!");
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
+
+app.post("/urls/:id/delete", (req, res) => {
+  console.log(urlDatabase);
+  console.log(req.params.id);
+  const shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
+app.post("/urls", (req, res) => {
+  console.log('req.body', req.body);
+  const newID = generateRandomString();
+  urlDatabase[newID] = req.body.longURL;
+  res.redirect(`/urls/${newID}`);
+});
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -41,10 +75,3 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n')
-});
-
-function generateRandomString() {
-  
-}
